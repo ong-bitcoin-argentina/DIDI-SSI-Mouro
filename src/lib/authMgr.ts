240ef9a0df62f09c6a9673356b823ca3e3070e0c
Daniel Obon <daniel.obon@creativecoefficient.net>
2020-01-21 18:50:09 -0300

    infura projectId

--- src/lib/authMgr.ts
+++ src/lib/authMgr.ts
@@ -1,98 +1,100 @@
-const didJWT = require('did-jwt')
-import { headersType } from './commonTypes';
+const didJWT = require("did-jwt");
+import { headersType } from "./commonTypes";
 
-export type AuthzConditionType ={
-    iss: string,
-    from?: string
-}
+export type AuthzConditionType = {
+	iss: string;
+	from?: string;
+};
 
 export type AuthDataType = {
-    user: string, //DID of the authenticated user.
-    authzRead?: AuthzConditionType[]  //Array of authz data for read
-    authzDelete?: AuthzConditionType[]  //Array of authz data for delete
-}
+	user: string; //DID of the authenticated user.
+	authzRead?: AuthzConditionType[]; //Array of authz data for read
+	authzDelete?: AuthzConditionType[]; //Array of authz data for delete
+};
 
 export type VerifiedJWTType = {
-    issuer: string,
-    payload: {
-        sub: string,
-        claim?: any
-    }
-}
+	issuer: string;
+	payload: {
+		sub: string;
+		claim?: any;
+	};
+};
 
 export class AuthMgr {
+	constructor() {
+		require("ethr-did-resolver").default({
+			rpcUrl: "https://mainnet.infura.io/v3/***REMOVED***"
+		});
+	}
 
-    constructor() {
-        require('ethr-did-resolver').default()
-    }
+	async verify(authToken: string): Promise<VerifiedJWTType> {
+		if (!authToken) throw new Error("no authToken");
+		const verifiedToken = await didJWT.verifyJWT(authToken);
+		return verifiedToken;
+	}
 
-    async verify(authToken: string): Promise<VerifiedJWTType> {
-        if (!authToken) throw new Error('no authToken')
-        const verifiedToken = await didJWT.verifyJWT(authToken);
-        return verifiedToken;
-   }
+	async verifyAuthorizationHeader(
+		headers: headersType
+	): Promise<VerifiedJWTType | null> {
+		let authHead = headers.authorization;
+		if (authHead === undefined) authHead = headers.Authorization;
+		if (authHead === undefined) return null;
 
-   async verifyAuthorizationHeader(headers: headersType):Promise<VerifiedJWTType|null>{
-    let authHead = headers.authorization;
-    if(authHead===undefined) authHead = headers.Authorization;
-    if(authHead===undefined) return null;
+		const parts = authHead.split(" ");
+		if (parts.length !== 2) throw Error("Format is Authorization: Bearer [token]");
+		const scheme = parts[0];
+		if (scheme !== "Bearer") throw Error("Format is Authorization: Bearer [token]");
 
-    const parts = authHead.split(" ");
-    if (parts.length !== 2) throw Error("Format is Authorization: Bearer [token]");
-    const scheme = parts[0];
-    if (scheme !== "Bearer") throw Error("Format is Authorization: Bearer [token]");
- 
-    return await this.verify(parts[1]);
-        
-   }
+		return await this.verify(parts[1]);
+	}
 
+	async getAuthData(headers: headersType): Promise<AuthDataType | null> {
+		//TODO: Check cache for headers.Authorization
 
-   async getAuthData(headers: headersType):Promise<AuthDataType | null>{
+		const authToken = await this.verifyAuthorizationHeader(headers);
+		if (authToken == null) return null;
 
-    //TODO: Check cache for headers.Authorization
+		let authData: AuthDataType = {
+			user: authToken.issuer
+		};
 
-    const authToken=await this.verifyAuthorizationHeader(headers);
-    if(authToken==null) return null;
-    
-    let authData:AuthDataType={
-        user: authToken.issuer,
-    }
+		if (authToken.payload.claim && authToken.payload.claim.access) {
+			let access: any[] = authToken.payload.claim.access;
+			let authzRead: AuthzConditionType[] = [];
+			let authzDelete: AuthzConditionType[] = [];
+			for (let i = 0; i < access.length; i++) {
+				const authzToken = access[i];
+				//Verify token
+				try {
+					//Decode token
+					const authZ = await this.verify(authzToken);
 
-    if(authToken.payload.claim && authToken.payload.claim.access){
-        let access: any[] = authToken.payload.claim.access;
-        let authzRead: AuthzConditionType[] = [];
-        let authzDelete: AuthzConditionType[] = [];
-        for(let i=0;i<access.length;i++){
-            const authzToken=access[i];
-            //Verify token
-            try{
-                //Decode token
-                const authZ=await this.verify(authzToken)
+					//Check if authZToken is issues to the right user
+					if (authZ.payload.sub == authData.user) {
+						const authzCond = {
+							iss: authZ.issuer,
+							...authZ.payload.claim.condition
+						};
 
-                //Check if authZToken is issues to the right user
-                if (authZ.payload.sub == authData.user){
-                    const authzCond={
-                        iss: authZ.issuer,
-                        ...authZ.payload.claim.condition
-                    }
+						switch (authZ.payload.claim.action) {
+							case "read":
+								authzRead.push(authzCond);
+								break;
+							case "delete":
+								authzDelete.push(authzCond);
+								break;
+						}
+					}
+				} catch (err) {
+					console.log(err.message + " -> " + authzToken);
+				}
+			}
 
-                    switch(authZ.payload.claim.action){
-                        case('read'): authzRead.push(authzCond);break;
-                        case('delete'): authzDelete.push(authzCond);break;
-                    }
-                }
+			if (authzRead.length > 0) authData.authzRead = authzRead;
+			if (authzDelete.length > 0) authData.authzDelete = authzDelete;
+		}
 
-            }catch(err){ console.log(err.message+' -> '+authzToken)}
-        }
-
-        if(authzRead.length>0) authData.authzRead=authzRead;
-        if(authzDelete.length>0) authData.authzDelete=authzDelete;
-    }
-
-    //TODO: Store Cache
-    return authData;
-   }
-
+		//TODO: Store Cache
+		return authData;
+	}
 }
-
-
