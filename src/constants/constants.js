cd0faf7598834a9a4bbce09760c1154f79236e13
fedelmar <fsuarez91@gmail.com>
2021-09-15 09:33:57 -0300

    simplify blockchain constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,16 +4,11 @@ const GAS_INCREMENT = process.env.GAS_INCREMENT || "1.1";
 
 // ======================================================================================================
 
-const BLOCKCHAIN_URL_MAIN = process.env.BLOCKCHAIN_URL_MAIN; // RSK
-const BLOCKCHAIN_URL_RSK = process.env.BLOCKCHAIN_URL_RSK; // RSK
-const BLOCKCHAIN_URL_LAC = process.env.BLOCKCHAIN_URL_LAC; // Lacchain
-const BLOCKCHAIN_URL_BFA = process.env.BLOCKCHAIN_URL_BFA; // BFA testnet
-
+// Blockchain
+const { BLOCKCHAIN_URL_RSK, BLOCKCHAIN_URL_LAC, BLOCKCHAIN_URL_BFA } = process.env;
+const { INFURA_KEY } = process.env;
 // uPort SC ON
-const BLOCKCHAIN_CONTRACT_MAIN = process.env.BLOCKCHAIN_CONTRACT_MAIN; // RSK
-const BLOCKCHAIN_CONTRACT_RSK = process.env.BLOCKCHAIN_CONTRACT_RSK; // RSK
-const BLOCKCHAIN_CONTRACT_LAC = process.env.BLOCKCHAIN_CONTRACT_LAC; // Lacchain
-const BLOCKCHAIN_CONTRACT_BFA = process.env.BLOCKCHAIN_CONTRACT_BFA; // BFA
+const { BLOCKCHAIN_CONTRACT_MAIN, BLOCKCHAIN_CONTRACT_LAC, BLOCKCHAIN_CONTRACT_BFA } = process.env;
 
 // Provider
 // MAINNET SHOULD BE THE FIRST NETWORK
@@ -24,28 +19,47 @@ const BLOCKCHAIN_CONTRACT_BFA = process.env.BLOCKCHAIN_CONTRACT_BFA; // BFA
 const PROVIDER_CONFIG = {
   networks: [
     {
-      name: "mainnet",
-      rpcUrl: BLOCKCHAIN_URL_MAIN,
+      name: 'mainnet',
+      rpcUrl: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
       registry: BLOCKCHAIN_CONTRACT_MAIN,
     },
     {
-      name: "lacchain",
+      name: 'ropsten',
+      rpcUrl: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
+      registry: BLOCKCHAIN_CONTRACT_MAIN,
+    },
+    {
+      name: 'rinkeby',
+      rpcUrl: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+      registry: BLOCKCHAIN_CONTRACT_MAIN,
+    },
+    {
+      name: 'goerli',
+      rpcUrl: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+      registry: BLOCKCHAIN_CONTRACT_MAIN,
+    },
+    {
+      name: 'kovan',
+      rpcUrl: `https://kovan.infura.io/v3/${INFURA_KEY}`,
+      registry: BLOCKCHAIN_CONTRACT_MAIN,
+    },
+    {
+      name: 'lacchain',
       rpcUrl: BLOCKCHAIN_URL_LAC,
       registry: BLOCKCHAIN_CONTRACT_LAC,
     },
     {
-      name: "bfa",
+      name: 'bfa',
       rpcUrl: BLOCKCHAIN_URL_BFA,
       registry: BLOCKCHAIN_CONTRACT_BFA,
     },
     {
-      name: "rsk",
+      name: 'rsk',
       rpcUrl: BLOCKCHAIN_URL_RSK,
-      registry: BLOCKCHAIN_CONTRACT_RSK,
+      registry: BLOCKCHAIN_CONTRACT_MAIN,
     },
   ],
 };
-
 
 module.exports = {
   BLOCKCHAIN: {
